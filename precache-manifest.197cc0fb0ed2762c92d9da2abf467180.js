42f9bed755834dadf1d8865618a31f21db20396f
muqsith <muqsithck@gmail.com>
2020-04-05 17:48:02 +0530

    Updates

---
+++ precache-manifest.197cc0fb0ed2762c92d9da2abf467180.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d672c34402504f02bc25293c3c87051",
+    "url": "/shopping-cart-demo/index.html"
+  },
+  {
+    "revision": "5312ad57149bba00df3b",
+    "url": "/shopping-cart-demo/static/css/main.e3023122.chunk.css"
+  },
+  {
+    "revision": "8cf7760036dbd62d4011",
+    "url": "/shopping-cart-demo/static/js/2.ff2613d0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/shopping-cart-demo/static/js/2.ff2613d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5312ad57149bba00df3b",
+    "url": "/shopping-cart-demo/static/js/main.b2a80bb3.chunk.js"
+  },
+  {
+    "revision": "d147d767b7ad744dd28f",
+    "url": "/shopping-cart-demo/static/js/runtime-main.33546835.js"
+  },
+  {
+    "revision": "5661caa01ae680c4573332220ce541de",
+    "url": "/shopping-cart-demo/static/media/Ahinsak.5661caa0.jpg"
+  },
+  {
+    "revision": "4ba45de345bb954eb337a41525c6e4c4",
+    "url": "/shopping-cart-demo/static/media/akiva.4ba45de3.jpg"
+  },
+  {
+    "revision": "341ea69f1081aa70e61f13035048d243",
+    "url": "/shopping-cart-demo/static/media/akshay.341ea69f.jpg"
+  },
+  {
+    "revision": "1436618c3f8727f12783fe8868183184",
+    "url": "/shopping-cart-demo/static/media/brigigowla.1436618c.jpg"
+  },
+  {
+    "revision": "c4b41160a994fc25f9ae3d432caefee9",
+    "url": "/shopping-cart-demo/static/media/brijmohan.c4b41160.jpg"
+  },
+  {
+    "revision": "950946725247d89e181d13b1ae0379a4",
+    "url": "/shopping-cart-demo/static/media/divya.95094672.jpg"
+  },
+  {
+    "revision": "d88a3e5201393914b6b2225bc9799f9c",
+    "url": "/shopping-cart-demo/static/media/iorganic1.d88a3e52.jpg"
+  },
+  {
+    "revision": "49c20a61ca74eed1ffc4f2048c5c4be3",
+    "url": "/shopping-cart-demo/static/media/iorganic500.49c20a61.jpg"
+  },
+  {
+    "revision": "7e637000de4cb1dab7e97fc3fdb8d246",
+    "url": "/shopping-cart-demo/static/media/jivika.7e637000.jpg"
+  },
+  {
+    "revision": "63a62df5591d2e7d4ecee88926d0e5f0",
+    "url": "/shopping-cart-demo/static/media/nanak.63a62df5.jpg"
+  },
+  {
+    "revision": "858a25d349ef5d46f6ae3af7a5d6f560",
+    "url": "/shopping-cart-demo/static/media/naturlle.858a25d3.jpg"
+  },
+  {
+    "revision": "6eaf958f4b1fca57f136078b02fddd18",
+    "url": "/shopping-cart-demo/static/media/shaiwal.6eaf958f.jpg"
+  },
+  {
+    "revision": "195bcedf21a4a68512cb9ecbc9e9ef5c",
+    "url": "/shopping-cart-demo/static/media/vanaliya.195bcedf.jpg"
+  },
+  {
+    "revision": "a97d6aed93122c7feaf6a3705826b75d",
+    "url": "/shopping-cart-demo/static/media/ved.a97d6aed.jpg"
+  }
+]);
